084290bfef981adba1d2eee0efd3e0f9dd974230
suryaraikumar-pixer <suryaraikumar5@gmail.com>
2020-12-24 21:59:32 +0530

    Database has created, blog app authenticated by admin

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render ##default one
-
+from .models import Post
 # from django.http import HttpResponse #created
 
-posts = [
-    {
-        'author': 'surya',
-        'title':  'Blog post',
-        'content': 'First Post content',
-        'date_posted': 'December 24, 2020'
-    },
-     {
-        'author': 'Jane Doe',
-        'title':  'Blog post 2',
-        'content': 'Second Post content',
-        'date_posted': 'December 24, 2020'
-    }
-]
+
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
